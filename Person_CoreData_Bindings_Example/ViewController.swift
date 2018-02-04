e59329584de8853da6464245748cb054925b24b4
Eric Kampman <erickampman@yahoo.com>
2018-02-04 09:15:25 -0800

    Initial Commit

---
+++ Person_CoreData_Bindings_Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Person_CoreData_Bindings_Example
+//
+//  Created by Eric Kampman on 2/4/18.
+//  Copyright © 2018 Eric Kampman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Person_CoreData_Bindings_ExampleTests/Person_CoreData_Bindings_ExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Person_CoreData_Bindings_ExampleTests.swift
+//  Person_CoreData_Bindings_ExampleTests
+//
+//  Created by Eric Kampman on 2/4/18.
+//  Copyright © 2018 Eric Kampman. All rights reserved.
+//
+
+import XCTest
+@testable import Person_CoreData_Bindings_Example
+
+class Person_CoreData_Bindings_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
